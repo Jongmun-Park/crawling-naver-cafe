57ad9d605f81d84a9608f9bcbf0014aeca7d85bf
jongmunSpecup <jongmun.park@specupad.com>
2018-12-13 17:38:04 +0900

    12.13 작업 : 거의 완성. 직무까지 잘 찍힘. // 스크립트 단에서 년월일 유효성 검사 추가하기. 14일 실제 테스트 예정

--- crawling_specup/crawling/views.py
+++ crawling_specup/crawling/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from urllib.parse import urljoin
 from django.http import JsonResponse
-from django.contrib import messages
 import mechanicalsoup
 import re
 import datetime
+# from django.contrib import messages
 
 def main_view(request):
     browser = mechanicalsoup.StatefulBrowser()
@@ -30,7 +30,8 @@ def data_list(request):
         'search.clubid': clubid,
         'search.menuid': menuid,
     }
-    data_list = []  # JsonResponse로 실어 보낼 배열 생성
+    data_list = []  # json_dict 사전에 담을 배열
+    json_dict = {} # JsonResponse로 실어 보낼 사전
 
     if(input_date == custom_today):     # 오늘 날짜를 검색했을 경우
         while(True):
@@ -41,112 +42,74 @@ def data_list(request):
             for tag in soup.select('.board_box'):   # li 태그의 클래스 네임 board_box로 접근
                 write_date = tag.find(class_='time').text   # 게시 날짜 추출
                 if len(write_date) == 5:    # 오늘 날짜 데이터 판별
-                    data_dict = {}
-                    title = tag.find("strong", class_='tit').text.strip()
-                    data_dict['title'] = title
-                    data_dict['writer'] = tag.find(class_='ellip').text
-                    try:
-                        article_url = urljoin(list_url, tag.find('a')['href'])
-                        data_dict['article_url'] = article_url
-                        article_soup = browser.open(article_url).soup
-                        try:
-                            compile_title = re.compile('^.*[★|\[](.*)[★|\]].*$')
-                            match_company = compile_title.match(title)
-                            company_name = match_company.group(1)
-                        except (AttributeError, IndexError) as err:          # 'NoneType' object has no attribute 'group' 에러 체크
-                            print('CompanyError: {0}'.format(err))
-                            company_name = ''
-                        try:
-                            date = article_soup.select('div.NHN_Writeform_Main div > span > b > span')[0].text
-                            job_sort = article_soup.select('div.NHN_Writeform_Main div > span > b > span')[1].text
-                            print(title, '!!!!!!date, job_sort!!!!', date, job_sort)
-                            compile_date = re.compile('^(.*)[~]\s(.*)$')
-                            match_date = compile_date.match(date)
-                            start_date = match_date.group(1)
-                            end_date = match_date.group(2)
-                        except (AttributeError, IndexError) as err:          # 'NoneType' object has no attribute 'group' 에러 체크
-                            print('DateError: {0}'.format(err))
-                            start_date = ''
-                            end_date = ''
-                            job_sort = ''
-                        data_dict['job_sort'] = job_sort
-                        data_dict['company'] = company_name
-                        data_dict['start_date'] = start_date
-                        data_dict['end_date'] = end_date
-                    except TypeError as err:       # 해당 날짜에 검색 결과 없을 때
-                        print('TypeError: {0}'.format(err))
-
-                    data_list.append(data_dict)
-
+                    data_list.append(crawling_data(tag, list_url, browser))
                 else:   # 검색 끝
-                    print('검색 종료')
-                    return JsonResponse(data_list, json_dumps_params={'ensure_ascii': True}, safe=False)
+                    json_dict['data_list'] = data_list
+                    json_dict['msg'] = '검색이 완료됐습니다.'
+                    return JsonResponse(json_dict, json_dumps_params={'ensure_ascii': True})
 
     else:   # 오늘 날짜 검색이 아닐 경우
-        correct = 0
+        correct = False
         while(True):
             search_page += 1
             print('page:', search_page, clubid, menuid)
             params['search.page'] = search_page
             soup = browser.open(list_url, params=params).soup
             if not soup.select('.board_box'):   # 검색 결과가 없는 경우 (맨 마지막 페이지까지 검색한 후)
-                print('검색 결과가 없습니다.')
-                return JsonResponse(data_dict, json_dumps_params={'ensure_ascii': True})
+                json_dict['msg'] = '검색이 완료됐습니다.'
+                return JsonResponse(json_dict, json_dumps_params={'ensure_ascii': True})
             else:
                 for tag in soup.select('.board_box'):
                     write_date = tag.find(class_='time').text
-                    if len(write_date) != 5 and (int(input_date[:2]) > int(write_date[:2])):    # 연도 필터링 : 해당 연도까지 검색하고, 결과값 없으면 검색 종료
-                        print(write_date, '검색 결과가 없습니다.')
-                        return JsonResponse(data_dict, json_dumps_params={'ensure_ascii': True})
-                    if input_date == write_date:  # 검색 값을 찾은 경우
-                        correct = 1
-                        data_dict = {}
-                        title = tag.find("strong", class_='tit').text.strip()
-                        data_dict['title'] = title
-                        data_dict['writer'] = tag.find(class_='ellip').text
-                        try:
-                            article_url = urljoin(list_url, tag.find('a')['href'])
-                            data_dict['article_url'] = article_url
-                            article_soup = browser.open(article_url).soup
-                            try:
-                                compile_title = re.compile('^.*[★|\[](.*)[★|\]].*$')
-                                match_company = compile_title.match(title)
-                                company_name = match_company.group(1)
-                            except (AttributeError, IndexError) as err:          # 'NoneType' object has no attribute 'group' 에러 체크
-                                print('CompanyError: {0}'.format(err))
-                                company_name = ''
-                            try:
-                                # test = article_soup.select('div.post_title')
-                                # if not test:
-                                #     print(article_url, '의 검색 결과 없음')
-                                # else:
-                                #     print('ok')
-                                date = article_soup.select('div.NHN_Writeform_Main div > span > b > span')[0].text
-                                job_sort = article_soup.select('div.NHN_Writeform_Main div > span > b > span')[1].text
-                                print(title, '!!!!!!date:', date, 'job_sort:', job_sort)
-                                compile_date = re.compile('^(.*)[~]\s(.*)$')
-                                match_date = compile_date.match(date)
-                                start_date = match_date.group(1)
-                                end_date = match_date.group(2)
-                            except (AttributeError, IndexError) as err:          # 'NoneType' object has no attribute 'group' 에러 체크
-                                print('DateError: {0}'.format(err))
-                                start_date = ''
-                                end_date = ''
-                                job_sort = ''
-                            data_dict['job_sort'] = job_sort
-                            data_dict['company'] = company_name
-                            data_dict['start_date'] = start_date
-                            data_dict['end_date'] = end_date
-                        except TypeError as err:       # 해당 날짜에 검색 결과 없을 때
-                            print('TypeError: {0}'.format(err))
-
-                        data_list.append(data_dict)
-
-                    elif correct == 1: # 검색 끝
-                        print('검색 종료')
-                        return JsonResponse(data_list, json_dumps_params={'ensure_ascii': True}, safe=False)
+                    if input_date == write_date:    # 검색 값을 찾은 경우
+                        correct = True
+                        data_list.append(crawling_data(tag, list_url, browser))
+                    elif correct == True:   # 검색 끝
+                        json_dict['data_list'] = data_list
+                        json_dict['msg'] = '검색이 완료됐습니다.'
+                        return JsonResponse(json_dict, json_dumps_params={'ensure_ascii': True})
+                    # 년월 필터링 : 해당 년월까지 검색하고, 결과값 없으면 검색 종료
+                    elif int(input_date[3:5]) > int(write_date[3:5]) and input_date[:2] == write_date[:2] and len(write_date) == 9:
+                        json_dict['msg'] = write_date + '까지 검색한 결과, 검색값이 없습니다..'
+                        return JsonResponse(json_dict, json_dumps_params={'ensure_ascii': True})
 
 
+# 데이터 크롤링 함수
+def crawling_data(tag, list_url, browser):
+    data_dict = {}
+    title = tag.find("strong", class_='tit').text.strip()
+    data_dict['title'] = title
+    data_dict['writer'] = tag.find(class_='ellip').text
+    try:
+        article_url = urljoin(list_url, tag.find('a')['href'])
+        data_dict['article_url'] = article_url
+        article_soup = browser.open(article_url).soup
+        try:
+            compile_title = re.compile('^.*[★|\[](.*)[★|\]].*$')
+            match_company = compile_title.match(title)
+            company_name = match_company.group(1)
+        except (AttributeError, IndexError) as err:          # 'NoneType' object has no attribute 'group' 에러 체크
+            print('CompanyError: {0}'.format(err))
+            company_name = ''
+        try:
+            date = article_soup.select('div.NHN_Writeform_Main div > span > b > span')[0].text
+            job_sort = article_soup.select('div.NHN_Writeform_Main div > span > b > span')[1].text
+            # print(title, '!!!!!!date:', date, 'job_sort:', job_sort)
+            compile_date = re.compile('^(.*)[~]\s(.*)$')
+            match_date = compile_date.match(date)
+            start_date = match_date.group(1)
+            end_date = match_date.group(2)
+        except (AttributeError, IndexError) as err:          # 'NoneType' object has no attribute 'group' 에러 체크
+            print('DateError: {0}'.format(err))
+            start_date = ''
+            end_date = ''
+            job_sort = ''
+        data_dict['job_sort'] = job_sort
+        data_dict['company'] = company_name
+        data_dict['start_date'] = start_date
+        data_dict['end_date'] = end_date
+    except TypeError as err:       # 해당 날짜에 검색 결과 없을 때
+        print('TypeError: {0}'.format(err))
 
-
+    return data_dict
 
